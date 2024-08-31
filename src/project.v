be761f3f9c39170e5df7cee80aa596f286aba3df
Brady Etz <17091220+b-etz@users.noreply.github.com>
2024-08-31 10:50:37 -0500

    Add files via upload
    First commit, added Verilog files from FPGA testing

--- src/project.v
+++ src/project.v
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-`default_nettype none
+`default_nettype wire
 
 module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
@@ -17,11 +17,21 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  assign uio_out[7:6] = 2'b0;
+  assign uio_out[1:0] = 2'b0;
+  assign uio_oe  = 8'b00111100;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
+  
+  tt08_morse_keyer tt_um_betz_morse_keyer(
+    .clk_i(clk), .rstn_i(rst_n),
+    .wpm_sel_i(ui_in[7:4]),                           // MSB = input[7], LSB = input[4]
+    .paddle_sel_i(ui_in[0]),  .iambic_AB_i(ui_in[1]), // Straight key / paddle selection = input[0],  Iambic-A/B selection = input[1]
+    .dit_i(uio_in[0]),        .dah_i(uio_in[1]),      // Inputs: Dit (active-high) = io[0], Dah (active-high) = io[1]
+    .aux_dit_o(uio_out[2]),   .aux_dah_o(uio_out[3]), // Outputs: Aux dit (active-high) = io[2], Aux dah (active-high) = io[3]
+	.aux_morse_o(uio_out[4]), .buzzer_o(uio_out[5]),  // Outputs: Morse Output (active-high) = io[4], Buzzer Output = io[5]
+    .seven_segment(uo_out)                            // Seven-segment display output uo_out[7:0] = 7seg[.GFEDCBA]
+  );
 
 endmodule
